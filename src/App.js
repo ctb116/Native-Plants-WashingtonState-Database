f69209103143f14e9839a42c04e1d49dbd707bc1
Catherine Bradley <catherinetybradley@gmail.com>
2018-12-18 14:43:05 -0800

    add mount rainer img with styling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './App.css';
 import FamilyPresentation from './components/FamilyPresentation';
 import PlantPresentational from './components/PlantPresentational';
 import NavBar from './components/NavBar';
@@ -7,8 +8,8 @@ import NavBar from './components/NavBar';
 class App extends Component {
   render() {
     return (
-      <div>
-        <div>
+      <div className="rainer-image">
+        <div className="tint">
           <NavBar/>
           <FamilyPresentation/>
           <PlantPresentational/>
